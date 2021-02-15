ba54a3932989a8944f2b1b97355cca26a243ab2c
Elena Rojas <erojas@growthaccelerationpartners.com>
2021-02-15 11:49:19 -0600

    initial configuration

---
+++ src/.prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  parser: 'flow',
+  printWidth: 120,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
+};
